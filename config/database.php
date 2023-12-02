bb08c4ea8c4bdadab04316f56427a48543e01aaf
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-02 11:41:51 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- config/database.php
+++ config/database.php
@@ -6,5 +6,16 @@
         private $pass = "";
         private $db_name = "php_pdo_oop_crud_mvc";
 
-        
+        public function makeConnection(){
+            $this->con -= null;
+
+            try {
+                $this->con = new PDO("mysql:host={$this->host}; dbname={$this->db_name}", $this->user, $this->pass);
+                $this->con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            }
+            catch (PDOException $e){
+                echo "Connection Lost : " . $e->getMessage();
+            }
+            return $this->con;
+        }
     }
