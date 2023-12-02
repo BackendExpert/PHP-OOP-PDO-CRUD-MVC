bc03ad9a030518ebb8d61120c2300d1fdc5965b0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-02 09:46:20 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/model/userModel.php
+++ app/model/userModel.php
@@ -8,6 +8,8 @@
         }
 
         public function read_all_user(){
-            
+            $all_users = "SELECT * FROM user_tbl";
+            $all_users_exc = $this->connect()->prepare($all_users);
+            $all_users_exc->execute();
         }
     }
