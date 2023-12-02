80bf8777d6fbc08bff1b11d6d7258678aa61c7f7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-02 11:35:40 +0530

    UPdate
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ config/database.php
@@ -0,0 +1,10 @@
+<?php
+
+    class Database{
+        private $host = "localhost";
+        private $user = "root";
+        private $pass = "";
+        private $db_name = "php_pdo_oop_crud_mvc";
+
+        
+    }
