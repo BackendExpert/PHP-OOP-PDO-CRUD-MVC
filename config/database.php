7bde1364f7148c00044ed618a9e8979ab4670cd5
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-02 10:21:32 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- config/database.php
+++ config/database.php
@@ -7,7 +7,7 @@
         private $db_name = "php_pdo_oop_crud_mvc";
 
         public function connect(){
-            $con = 'mysql:host' . $this->host . 'dbname =' . $this->db_name;
+            $con = 'mysql:host=' . $this->host . ';dbname=' . $this->db_name;
             $pdo = new PDO($con, $this->user, $this->pass);
             $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             return $pdo;
