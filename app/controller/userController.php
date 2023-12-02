bb291d57267c083cd534cecffaf4995f31d69ac5
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-02 11:57:51 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/controller/userController.php
+++ app/controller/userController.php
@@ -4,5 +4,16 @@
     require_once("../model/userModel.php");
 
     class userController {
+        private $db;
+        private $userModel;
 
+        public function __construct(){
+            $this->db = new Database;
+            $this->userModel = new UserModel($this->db->showAllUsers());
+        }
+
+        public function index(){
+            $users = $this->userModel->showAllUsers();
+            include('../view/index.php');
+        }
     }
