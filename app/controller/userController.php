1ae469829e4c267cef2c1b30f0778c1eba5b084d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-02 09:56:15 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/controller/userController.php
+++ app/controller/userController.php
@@ -4,4 +4,13 @@
 
     function users_show_all(){
         $show_users = new userM();
+        $exc_user = $show_users->read_all_user();
+
+        if($exc_user){
+            $users_arrary = array();
+            while($row = $show_users->dba_fetch()){
+                $users_arrary[] = $row;
+            }
+            return $users_arrary;
+        }
     }
