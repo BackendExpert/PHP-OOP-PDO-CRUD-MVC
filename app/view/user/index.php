f725e95b3cc59f3022ef94be769ecc71cf19a532
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-02 09:16:53 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/view/user/index.php
+++ app/view/user/index.php
@@ -1,3 +1,31 @@
 <?php 
     include("../../../headers/header.php");
-    
+
+?>
+
+<style>
+    .table{
+        margin: 50px 0;
+    }
+</style>
+
+<div class="container">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Fisrst Name </th>
+                <th>Last Name </th>
+                <th>Mobile Number </th>
+                <th>Address </th>
+                <th>Join Date </th>
+                <th>Last Update </th>
+                <th>Action </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
